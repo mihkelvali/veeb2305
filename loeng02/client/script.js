321d68978a0c3698608bfe30503b282078642ce1
Mihkel Väli <mihkel.vali@hotmail.com>
2023-05-22 19:11:52 +0300

    kodune 02

--- loeng02/client/script.js
+++ loeng02/client/script.js
@@ -6,11 +6,18 @@ fetch('http://localhost:8000')
   })
   .then((data) => {
     console.log('õnnestus', data);
-    app.innerHTML = `
-      Eesnimi: ${data.eesnimi}<br>
-      Perenimi: ${data.perenimi}<br>
-      <img src="${data.imageUrl}">
-    `
+    for (let matk of data) {
+      console.log(matk);
+      app.innerHTML += `
+        <div class="matk">
+          <h2>${matk.title}</h2>
+          <div>Algusaeg: ${matk.startTime}</div>
+          <div>${matk.description}</div>
+          <a href="https://www.google.com/maps/@${matk.lat},${matk.long},17z" target="_blank">Koguneme siin!</a><br>
+          <img src="${matk.imageUrl}">
+        </div>
+      `;
+    }
   })
   .catch(function() {
     // handle the error
--- loeng02/server/index.js
+++ loeng02/server/index.js
@@ -3,23 +3,23 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
-let inimene = {
-  eesnimi: 'Mihkel',
-  perenimi: 'Väli',
-  kasElus: true
-}
-
 let matk1 = {
-  title: 'mingi pealkiri',
-  imageUrl: '',
-  description: '',
+  title: 'Matk ümber Ülemiste',
+  imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipORwhgR234cnj9UsRg1wZAArFXZUHPKUui24bc7=w300-h200-k-no',
+  description: 'Jalga panna kõrge säärega saapad!',
+  startTime: new Date(2023, 4, 27, 10, 0, 0),
+  lat: 59.3763953,
+  long: 24.7488814,
   participants: ['Mihkel']
 }
 
 let matk2 = {
-  title: 'mingi pealkiri',
-  imageUrl: '',
-  description: '',
+  title: 'Merematk ümber Suurupi poolsaare',
+  imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipN7PyVsqrmPCDXjPRRaNJ5XyPmTkTR3z9baUoiZ=w300-h200-k-no',
+  description: 'Alustame Lohusalust, lõpetame Kakumäel',
+  startTime: new Date(2023, 5, 24, 19, 30, 0),
+  lat: 59.4038059,
+  long: 24.2013333,
   participants: ['Mihkel']
 }
 
