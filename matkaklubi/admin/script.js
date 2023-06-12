a0c621c00b9d089a66a70b1483605394019300a3
Mihkel Väli <mihkel.vali@hotmail.com>
2023-06-12 19:22:40 +0300

    Loeng 9 matkade muutmine ja kustutamine

--- matkaklubi/admin/script.js
+++ matkaklubi/admin/script.js
@@ -1,5 +1,5 @@
-// const API_URL = 'https://mihkli-matkad-app.onrender.com';
-const API_URL = 'http://localhost:10000';
+const API_URL = 'https://mihkli-matkad-app.onrender.com';
+// const API_URL = 'http://localhost:10000';
 const matkad = document.querySelector('#matkad');
 const uusMatk = document.querySelector('#uus-matk');
 const matkaPaneeliPealkiri = document.querySelector('#matka-paneel-pealkiri');
@@ -11,6 +11,7 @@ const uusMatkPildiUrl = document.querySelector('#uus-matk-pildi-url');
 const uusMatkAlgus = document.querySelector('#uus-matk-algus');
 const uusMatkLopp = document.querySelector('#uus-matk-lopp');
 const uusMatkKirjeldus = document.querySelector('#uus-matk-kirjeldus');
+const nupud = document.querySelector('#nupud');
 const salvestaMatkNupp = document.querySelector('#lisa-matk-nupp');
 let treks = [];
 matkad.innerHTML = 'Loading...';
@@ -21,6 +22,11 @@ function startCreatingNewTrek() {
   matkaPaneeliPealkiri.innerHTML = 'Lisa uus matk';
   salvestaMatkNupp.innerHTML = 'Lisa matk';
   salvestaMatkNupp.onclick = function() { addTrek() };
+
+  const kustutaNupp = document.querySelector('#kustuta-nupp');
+  if (kustutaNupp !== null) {
+    kustutaNupp.remove();
+  }
 
   uusMatk.style.display = 'flex';
 }
@@ -34,10 +40,37 @@ function editTrek(trekId) {
 
   matkaPaneeliPealkiri.innerHTML = 'Matka muutmine';
 
-  uusMatkNimi.value = 'mingi asi';
+  uusMatkNimi.value = trekToEdit.name;
+  uusMatkLaiuskraad.value = trekToEdit.latitude;
+  uusMatkPikkuskraad.value = trekToEdit.longitude;
+  uusMatkHind.value = trekToEdit.price;
+  uusMatkPildiUrl.value = trekToEdit.image_url;
+  uusMatkAlgus.value = trekToEdit.start_time;
+  uusMatkLopp.value = trekToEdit.end_time;
+  uusMatkKirjeldus.value = trekToEdit.description;
 
   salvestaMatkNupp.innerHTML = 'Salvesta muudatused';
   salvestaMatkNupp.onclick = function() { saveTrekChanges(trekId) };
+
+  let kustutaNupp = document.querySelector('#kustuta-nupp');
+  if (kustutaNupp !== null) {
+    kustutaNupp.remove();
+  }
+
+  kustutaNupp = document.createElement('button');
+  kustutaNupp.id = 'kustuta-nupp';
+  kustutaNupp.innerHTML = 'Kustuta matk';
+  kustutaNupp.onclick = function() {
+    if (!confirm('Kas oled kindel, et tahad matka kustutada?')) {
+      return;
+    }
+    deleteTrek(trekId);
+    closeNewTrekPanel();
+  };
+  kustutaNupp.addEventListener('click', function(event) {
+    event.preventDefault();
+  });
+  nupud.appendChild(kustutaNupp);
 
   uusMatk.style.display = 'flex';
 }
@@ -47,13 +80,46 @@ function closeNewTrekPanel() {
   emptyAllInputs();
 }
 
-async function saveTrekChanges(trekId) {
-  console.log('savetrekchanges', trekId);
-  // funktsioon, kus tehakse treki uuendamise päring
-  // kontrollime, ega ükski väli tühi ei ole
+async function deleteTrek(trekId) {
+  try {
+    await fetch(`${API_URL}/api/treks/${trekId}`, {
+      method: 'DELETE'
+    });
 
-  // teeme PUT päringu, kus saadame ID ja sellele ID'le vastavad treki väärtused
-  // await fetch(`${API_URL}/api/treks/${trekId}`, { method: 'PUT', ...})
+    getTreks();
+  } catch(e) {
+    console.log(e);
+    matkad.innerHTML = 'Error, couldn\'t fetch treks';
+  }
+}
+
+async function saveTrekChanges(trekId) {
+  if (isAnyInputEmpty()) {
+    return;
+  }
+  try {
+    await fetch(`${API_URL}/api/treks/${trekId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: uusMatkNimi.value,
+        latitude: uusMatkLaiuskraad.value,
+        longitude: uusMatkPikkuskraad.value,
+        price: uusMatkHind.value,
+        image_url: uusMatkPildiUrl.value,
+        start_time: uusMatkAlgus.value,
+        end_time: uusMatkLopp.value,
+        description: uusMatkKirjeldus.value,
+      })
+    }).then((response => response.json()));
+
+    getTreks();
+  } catch(e) {
+    console.log(e);
+    matkad.innerHTML = 'Error, couldn\'t fetch treks';
+  }
 }
 
 async function addTrek() {
@@ -71,9 +137,9 @@ async function addTrek() {
         latitude: uusMatkLaiuskraad.value,
         longitude: uusMatkPikkuskraad.value,
         price: uusMatkHind.value,
-        imageUrl: uusMatkPildiUrl.value,
-        startTime: uusMatkAlgus.value,
-        endTime: uusMatkLopp.value,
+        image_url: uusMatkPildiUrl.value,
+        start_time: uusMatkAlgus.value,
+        end_time: uusMatkLopp.value,
         description: uusMatkKirjeldus.value,
       })
     }).then((response => response.json()));
@@ -96,7 +162,7 @@ async function getTreks() {
     `;
     for (let trek of treks) {
       matkad.innerHTML += `
-        <a onclick="editTrek(${trek.id})" href="javascript:;">${trek.name}</a>
+        <a onclick="editTrek(${trek.id})" href="javascript:;">${trek.name} ${trek.id}</a>
       `;
     }
     matkad.innerHTML += `
